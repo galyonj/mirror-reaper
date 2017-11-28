6b70acb2bd50a0b6ebf4476b421bc06d87783235
John Galyon <galyon.jb@gmail.com>
2017-11-27 19:51:54 -0500

    Updates to GAS.

--- site/web/app/themes/galyon/header.php
+++ site/web/app/themes/galyon/header.php
@@ -32,15 +32,16 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
 
 		<?php wp_head(); ?>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-88932614-1"></script>
 		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-					(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
 
-			ga('create', 'UA-88932614-1', 'auto');
-			ga('send', 'pageview');
+			gtag('config', 'UA-88932614-1');
 		</script>
+
 	</head>
 	<body <?php body_class(); ?> data-scroll-id="page-top" id="page-top">
 		<div class="wrapper">
